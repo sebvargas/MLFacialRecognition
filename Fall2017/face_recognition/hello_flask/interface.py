553471d0b396d316a7e7439f4c93f4c7fe8fd217
Elijah Michaelson <elijahmichaelson@gmail.com>
2018-05-01 21:45:05 -0700

    more interface work

--- Fall2017/face_recognition/hello_flask/interface.py
+++ Fall2017/face_recognition/hello_flask/interface.py
@@ -12,31 +12,26 @@ def main():
     for i in range(1,len(sys.argv)):
         print str(sys.argv[i])
     '''
-    test = sys.argv[1] + sys.argv[2]
-    with open('backend_out', 'a') as the_file:
-        the_file.write(test)
-
-    return "str"
-
+    output = "output"
+    
     if (sys.argv[1] == 'register'):
         username = sys.argv[2]
         imgs = sys.argv[3:]
         if backend.register(username,imgs):
-            print 'success'
-            return 0
+            output = 'success'
         else:
-            print 'register failed'
-            return -1
+            output = 'register failed'
+            
     elif (sys.argv[1] == 'login'):
         imgs = sys.argv[2:]
         username = backend.login(imgs,MEDIUM_SECURITY)
-        if username:
-            with open('backend_out', 'a') as the_file:
-                the_file.write(username)
+        if not username:
+            output = "NONE FOUND"
         else:
-            print 'no user found'
-            return -1
+            output = username
     else:
-        print "failed"
-        return -1
+        output = "failed"
+    
+    with open('backend_out', 'a') as the_file:
+            the_file.write(output)
 main()
